a3bc4755cb7af22b12d1587dde66ea180f05ddb3
Nipun Jain <nipun.jain@oriserve.com>
2021-02-07 22:47:13 +0530

    shipped to ES with auth succesfully, todo: ssl, array of hosts, elasticclient, ilm

--- elastic-logger/createErrorLogInElastic.js
+++ elastic-logger/createErrorLogInElastic.js
@@ -1,29 +1,38 @@
-const momentTimezone = require('moment-timezone');
+// const momentTimezone = require('moment-timezone');
 const { bulkIndex } = require('./crudApi');
-// const TIMEZONE = "Asia/Calcutta";
-
+const { errorHandler } = require('./errorHandler');
 let errorLogRequestBatch = [];
 
-const createErrorLogInElastic = async (err, url, microServiceName, brand_name, cs_env, batchSize = 10, TIMEZONE = 'Asia/Calcutta') => {
+const createErrorLogInElastic = async ({ err, url, authType, auth, microServiceName, brand_name, cs_env, batchSize = 10, timezone = 'Asia/Calcutta', scope }) => {
   try {
-    let date = momentTimezone().tz(TIMEZONE).startOf('day').format('YYYY-MM-DD');
-    let dateTime = momentTimezone().tz(TIMEZONE).format();
-
-    let log = {
-      title: err.stack.split("\n")[0],
-      description: err.stack,
-      logDate: date,
-      logDateTime: dateTime,
-      "@timestamp": dateTime
-    };
-
-    if (errorLogRequestBatch.length >= batchSize) {
-      let index = microServiceName + '_' + brand_name + '_' + cs_env;
-      bulkIndex(errorLogRequestBatch, index, url);
-      errorLogRequestBatch = [];
-    };
+    if (!err || !url || !authType || !microServiceName || !brand_name || cs_env) {
+      if (authType == 'none' && !auth) { //testing needed
+        throw new Error('Arguments missing: elastic-logger not initialized.', err);
+      }
+    }
+    scope = scope ? scope : 'manualFunctionCall';
+    const log = await errorHandler({ err, type: 'nodejs', ship: true, timezone: timezone, scope });
+    // console.log('morphed log---->', log);
     errorLogRequestBatch.push(log);
-  } catch (error) {
+    // if (errorLogRequestBatch.length >= batchSize) {
+    const index = brand_name + '_' + microServiceName + '_' + cs_env;
+    if (authType == 'none') {
+      bulkIndex(errorLogRequestBatch, index, url, authType, null);
+      errorLogRequestBatch = [];
+    } else if (authType == 'basic') {
+      bulkIndex(errorLogRequestBatch, index, url, authType, auth);
+      errorLogRequestBatch = [];
+    } else if (authType == 'api') { //api
+      // bulkIndex(errorLogRequestBatch, index, url, authType, auth);
+      // errorLogRequestBatch = [];
+    } else {
+      throw Error('Invalid authType specified: Allowed values are: none, basic, api');
+    }
+    // };
+    // errorLogRequestBatch.push(log);
+  } catch (err) {
+    // console.log('errrr->', err);
+    errorHandler({ err, type: 'nodejs', ship: true, scope: '@niccsj/elastic-logger.createErrorLogInElastic' });
   }
 }
 
--- elastic-logger/crudApi.js
+++ elastic-logger/crudApi.js
@@ -1,41 +1,37 @@
 const axios = require('axios');
-// const uuidv4 = require('uuid/v4');
+const { errorHandler } = require('./errorHandler');
 
-// const indexDocument = async (data, index, url) => {
-//     axios({
-//         method: 'PUT',
-//         headers: { 'content-type': 'application/json' },
-//         url: url + '/' + index + '/_doc/' + uuidv4(),
-//         data: data
-//     }).then(resp => {
-//     }).catch(err => {
-//         console.error("In elastic-logger(err) ---> ", err);
-//     });
-// }
-
-const bulkIndex = async (data, index, url) => {
+const bulkIndex = async (data, index, url, authType, auth = null) => {
     data = formatBulkLogs(data);
     index = index.toLowerCase();
+    console.log('data after--->', data);
     axios({
         method: 'POST',
-        headers: { 'content-type': 'application/json' },
+        headers: {
+            'content-type': 'application/json',
+        },
         url: url + '/' + index + '/_bulk',
+        auth: authType == 'basic' ? { 'username': 'niccsj', 'password': '/8ra{8+K2fxcE^H2' } : authType == 'api' ? 'api' : null,
         data: data
     }).then(resp => {
     }).catch(err => {
-        console.error("In elastic-logger(err) ---> ", err);
+        errorHandler({ err, type: 'axios', ship: false, scope: '@niccsj/elastic-logger.bulkIndex' });
     });
 }
 
 const formatBulkLogs = data => {
-    let bulkData = "";
-    data.map(log => {
-        bulkData += `{"index": {}}\n${JSON.stringify(log)}\n`
-    });
-    return bulkData;
+    try {
+        console.log('formatData--->', data);
+        let bulkData = "";
+        data.map(log => {
+            bulkData += `{"index": {}}\n${JSON.stringify(log)}\n`
+        });
+        return bulkData;
+    } catch (err) {
+        errorHandler({ err, type: 'nodejs', ship: false, scope: '@niccsj/elastic-logger.formatBulkLogs' });
+    }
 }
 
 module.exports = {
-    // indexDocument,
     bulkIndex,
 }
