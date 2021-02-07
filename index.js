00c939b6c4554016831fb8faec7cdba758702a6c
Nipun Jain <nipun.jain@oriserve.com>
2021-02-07 13:05:09 +0530

    added dependencies, updated readme.md, inital code in working condition

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const initial = (input) => { 
-    console.log('outpout');
-};
+const { indexDocument, bulkIndex} = require('./elastic-logger/crudApi');
+const { exportAccessLogs} = require('./elastic-logger/exportAccessLogs');
+const { overwriteHttpProtocol } = require('./elastic-logger/outGoingApiLogger');
+const { createErrorLogInElastic } = require('./elastic-logger/createErrorLogInElastic');
 
 module.exports = {
-    initial
-};
+    indexDocument,
+    bulkIndex,
+    exportAccessLogs,
+    overwriteHttpProtocol,
+    createErrorLogInElastic,
+}
