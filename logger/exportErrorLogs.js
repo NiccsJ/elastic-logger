75ac7419d1493c37b619f2a2e3d834ea06f0d90f
Nipun Jain <nipun.jain@oriserve.com>
2021-04-15 14:49:11 +0530

    code refactore, critical infinite loop bug fix

--- logger/exportErrorLogs.js
+++ logger/exportErrorLogs.js
@@ -1,5 +1,5 @@
-const { checkSuppliedArguments, shipDataToElasticsearh } = require('../utils/utilities');
-const { errorHandler, elasticError } = require('../utils/errorHandler');
+// const { checkSuppliedArguments, shipDataToElasticsearh } = require('../utils/utilities');
+const { errorHandler } = require('../utils/errorHandler');
 
 /**
  * Initiates the `error logger`
@@ -20,13 +20,10 @@ const { errorHandler, elasticError } = require('../utils/errorHandler');
 
 const exportErrorLogs = async ({ err, microServiceName, brand_name, cs_env, batchSize, timezone = 'Asia/Calcutta', scope = 'global', status = null, metadata, ship = true, log = true }) => {
     try {
-        const logObj = await errorHandler({ err, ship, log, timezone, scope, status, exporter: true, brand_name, cs_env, microServiceName, metadata});
-        shipDataToElasticsearh({ log: logObj, microServiceName, brand_name, cs_env, batchSize, timezone, exporterType: 'error' });
+        errorHandler({ err, ship, log, timezone, scope, status, exporter: true, brand_name, cs_env, microServiceName, metadata});
     } catch (err) {
-        errorHandler({ err, ship: true, scope: '@niccsj/elastic-logger.exportErrorLogs' });
+        errorHandler({ err, self: true, ship: false, scope: '@niccsj/elastic-logger.exportErrorLogs' });
     }
 }
 
-module.exports = {
-    exportErrorLogs
-}
+module.exports = { exportErrorLogs };
