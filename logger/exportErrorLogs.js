02f7d692f51f39c948c5d1d2e82c0be404b0d883
Nipun Jain <nipun.jain@oriserve.com>
2021-04-17 11:30:28 +0530

    removed console

--- logger/exportErrorLogs.js
+++ logger/exportErrorLogs.js
@@ -21,7 +21,6 @@ const exportErrorLogs = async ({ err, microServiceName, brand_name, cs_env, batc
     try {
         // const logObj = await 
         errorHandler({ err, ship, log, timezone, scope, status, exporter: true, batchSize, brand_name, cs_env, microServiceName, metadata });
-        // console.log('babababbababa<<><><><><><><><><><><><>', logObj);
         // shipDataToElasticsearh({ log: logObj, microServiceName, brand_name, cs_env, batchSize, timezone, exporterType: 'error' });
     } catch (err) {
         errorHandler({ err, self: true, ship: false, scope: '@niccsj/elastic-logger.exportErrorLogs' });
